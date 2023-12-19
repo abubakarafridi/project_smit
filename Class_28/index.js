4a76329d6c5ce2e2b2ea7118527bdda0c509b1f2
abubakarafridi <abubakarafridi407@gmail.com>
2023-12-19 11:24:15 +0500

    done with class 28,29

---
+++ Class_28/index.js
@@ -0,0 +1,18 @@
+alert("Thanks for your input");
+
+// JAVASCRIPT CLASS-00
+
+// we need language to communicate
+// what is a programming language?
+// machine language
+// language grammar, syntax
+// ASCII table
+// binary number system
+// decimal to binary conversion
+// hello world in js using vs code
+// hello world in JS using the browser console
+// hello world in JS using replit.com
+// hello world in JS using Programiz JS IDE
+// alerts in js
+// strings in js
+// finished the first chapter "ALERTS" exercises in class
---
+++ Class_29/number.js
@@ -0,0 +1,34 @@
+// JAVASCRIPT CLASS-01
+
+// CHAPTER-3
+// variables for numbers
+// RULES FOR NAMING VARIABLES https://www.w3schools.com/js/js_variables.asp
+// number variables for math calculation
+// addition vs concatenation
+
+let weight = 150;
+
+weight + 25;
+
+console.log(weight);
+
+let originalNum = 23;
+let newNum = originalNum + 7;
+
+console.log(newNum);
+
+var originalNumber = 23;
+var numToBeAdded = 7;
+var newNumber = originalNumber + numToBeAdded;
+
+console.log(newNumber);
+
+var originalNumber1st = 90;
+originalNumber1st = originalNumber1st + 10;
+
+console.log(originalNumber1st);
+
+var originalNumber2nd = "23";
+var newNumber2nd = originalNumber2nd + 7;
+
+console.log(newNumber2nd);
