ce53d65b98e1beb7da1878d0ee8135cb034dbf66
abubakarafridi <abubakarafridi407@gmail.com>
2024-01-02 23:46:45 +0500

    class 33 done

---
+++ js/Class_33/array-add-remove.js
@@ -0,0 +1,36 @@
+// CHAPTER-16 ADDING AND REMOVING ELEMENTS
+
+// const books = ["computer science", "mathematics"];
+
+// add element to the array
+
+// single
+// books.push("physics");
+// console.log(books);
+
+// multiple
+// books.push("physics", "chemistry");
+// console.log(books);
+
+// add element at a specifi position/index, it will replace already existing element
+// books[1] = "physics";
+// books[2] = "physics";
+// console.log(books);
+
+// adding elements to empty array
+// const books = [];
+
+// books[0] = "computer";
+// books[1] = "urdu";
+// books[2] = "english";
+// books[10] = "math";
+
+// console.log(books);
+
+// remove elements
+// const books = ["urdu", "english", "math"];
+// books.pop();
+// const poppedBook = books.pop();
+
+// console.log(books);
+// console.log({ poppedBook })
---
+++ js/Class_33/array.js
@@ -0,0 +1,42 @@
+// CHAPTER-15: ARRAYS
+
+// problem: for multiple values we would have to create multiple variables/containers
+// const fruit0 = 'mango'
+// const fruit1 = 'banana'
+// const fruit2 = 'strawbarray'
+
+// to solve the above problem, we introduced the concept of arrays
+// so, array is a container that can store multiple values
+
+// create array
+// const fruits = [];
+// console.log(fruits);
+
+// concept
+// const hotel = ["Ali", "Bilal", "Choudhry", "Danyal", "Faisal"];
+// console.log(hotel);
+
+// using emojis
+// const fruits = ["🍌", "🍎", "🥭", "🍊", "🍓", "🍐"];
+// const animal = ["🐱", "🐶", "🐄", "🐀", "🐅"];
+// console.log(fruits);
+// console.log(animal);
+// document.write(animal);
+
+// putting elements in an array
+// const fruits = ["banana", "Mango", "Apple", "Orange", "Strawberry"];
+
+// accessing array elements, whole array
+// console.log(fruits);
+
+// accessing specific array elements
+// const fruits = ["🍌", "🍎", "🥭", "🍊", "🍓", "🍐"];
+// const animals = ["🐱", "🐶", "🐄", "🐀", "🐅"];
+
+// console.log(fruits[2]);
+
+// mix different types
+// const mixedData = [1, "hello", true, 3.5, "Bye"];
+// console.log(mixedData);
+
+// array in js are dynamic
